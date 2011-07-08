35bc860925e7a7cf268cf3c10d15d8cab552fead
junkafarian <fergus.doyle@largeblue.com>
2011-07-08 18:02:24 +0100

    Cut release for 0.2.4

---
+++ CHANGES.rst
@@ -0,0 +1,13 @@
+CHANGES
+=======
+
+0.2.4 (2011-07-08)
+------------------
+
+- Fixed dependency on pivotal-py to >=0.1.2
+
+0.2.3 (2011-07-07)
+------------------
+
+- A bugfix release due to bad packaging when uploading to PyPI. Sorry to anyone who experienced problems!
+
--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 
 REQUIRES = [
     'httplib2',
-    'pivotal-py',
+    'pivotal-py>=0.1.2',
     ]
 
 setup(
     name='bushy',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    version='0.3dev',
+    version='0.2.4',
     author='junkafarian',
     author_email='junkafarian@gmail.com',
     url='https://github.com/junkafarian/bushy',
@@ -27,7 +27,7 @@ setup(
         "Environment :: Console",
     ],
     description='A git workflow plugin.',
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst').read() + open('CHANGES.rst').read(),
     include_package_data=True,
     entry_points='''
 [console_scripts]
